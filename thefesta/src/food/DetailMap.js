cfee5fc3f361dcbba6eb85d6df360ed23645bdd1
PMR <12121db@gmail.com>
2023-12-05 23:32:48 +0900

    test commit

--- thefesta/src/food/DetailMap.js
+++ thefesta/src/food/DetailMap.js
@@ -21,7 +21,7 @@ const DetailMap = ({ mapx, mapy }) => {
 
             const imageSrc = '/images/foodshop.png', // 마커이미지 주소    
                 imageSize = new kakao.maps.Size(64, 69), // 마커이미지 크기
-                imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지 옵션(마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정)
+                imageOption = { offset: new kakao.maps.Point(35, 60) }; // 마커이미지 옵션(마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정)
 
             // 마커의 이미지정보를 가지고 있는 마커이미지를 생성
             const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
--- thefesta/src/food/Detailfood.js
+++ thefesta/src/food/Detailfood.js
@@ -139,11 +139,24 @@ function Detailfood() {
     };
 
     useEffect(() => {
+        // 페이지 맨 위로 스크롤
+        window.scrollTo(0, 0);
+
+        // 음식 및 사용자 정보 가져오기
         getFood();
         getUserInfo();
-        // 컴포넌트가 마운트 될 때, 로컬 스토리지에서 좋아요 상태를 불러와 설정
+
+        // 로컬 스토리지에서 좋아요 상태 불러오기
         setIsLiked(getLikedStatusFromLocalStorage(userId, contentid));
     }, [userId, contentid]);
+
+    // 주소, 전화, 영업시간, 휴무일, 주차시설, 대표메뉴, 취급메뉴 데이터에서 <br>을 공백으로 대체
+    const formatDataWithLineBreaks = (data) => {
+        if (typeof data === 'string') {
+            return data.replace(/<br>/g, ' ');
+        }
+        return data;
+    };
 
     // 좋아요 상태 변경 시 로컬 스토리지에 상태 업데이트
     useEffect(() => {
@@ -168,31 +181,31 @@ function Detailfood() {
                             <tbody>
                                 <tr>
                                     <th>주소</th>
-                                    <td>{food.addr1}</td>
+                                    <td>{formatDataWithLineBreaks(food.addr1)}</td>
                                 </tr>
                                 <tr>
                                     <th>전화</th>
-                                    <td>{food.infocenterfood}</td>
+                                    <td>{formatDataWithLineBreaks(food.infocenterfood)}</td>
                                 </tr>
                                 <tr>
                                     <th>영업시간</th>
-                                    <td>{food.opentimefood}</td>
+                                    <td>{formatDataWithLineBreaks(food.opentimefood)}</td>
                                 </tr>
                                 <tr>
                                     <th>휴무일</th>
-                                    <td>{food.restdatefood}</td>
+                                    <td>{formatDataWithLineBreaks(food.restdatefood)}</td>
                                 </tr>
                                 <tr>
                                     <th>주차시설</th>
-                                    <td>{food.parkingfood}</td>
+                                    <td>{formatDataWithLineBreaks(food.parkingfood)}</td>
                                 </tr>
                                 <tr>
                                     <th>대표메뉴</th>
-                                    <td>{food.firstmenu}</td>
+                                    <td>{formatDataWithLineBreaks(food.firstmenu)}</td>
                                 </tr>
                                 <tr>
                                     <th>취급메뉴</th>
-                                    <td>{food.treatmenu}</td>
+                                    <td>{formatDataWithLineBreaks(food.treatmenu)}</td>
                                 </tr>
                             </tbody>
                         </table>
