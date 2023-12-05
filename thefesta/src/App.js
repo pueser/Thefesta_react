cd8dc62f6d37da22978b5c4aab1bacae2a866ae8
PJH <qkrwlgus6913@naver.com>
2023-12-05 11:29:36 +0900

    충돌 수정

--- thefesta/src/App.js
+++ thefesta/src/App.js
@@ -2,16 +2,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from "./main/Footer";
 import Header from "./main/Header";
 import Detailfood from './food/Detailfood';
-import List from './food/List';
 import Login from './food/Login';
 import Scheduler from './scheduler/scheduler';
-<<<<<<< HEAD
-import Detail from './festival/Detail';
-import Main from './festival/Main';
-
-=======
 import Main from './main/Main';
->>>>>>> 659e923005355a8a3b4ad049080ab98928dd6dc7
+
 function App() {
   return (
     <BrowserRouter>
@@ -21,13 +15,7 @@ function App() {
         <Route path='/food/detail/:contentid' element={<Detailfood />} />
         <Route path='/member/loginPost' element={<Login />} />
         <Route path='/scheduler' element={<Scheduler />} />
-<<<<<<< HEAD
         <Route path='/' element={<Main />} />
-=======
-        <Route path='/festival/detail/:contentid' element={<Detail />} />
-        <Route path='/festival/list' element={<Main />} />
-       
->>>>>>> 6977dbb7656f4a7c521dd5aff9b1ea22987863bb
       </Routes>
       <Footer />
     </BrowserRouter>
