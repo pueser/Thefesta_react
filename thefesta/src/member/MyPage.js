bb546ce4d4589ba70b1c501e62f6386d977c3f0a
ekdlsdlsep <dain7362@gmail.com>
2023-12-14 14:33:27 +0900

    회원 수정

--- thefesta/src/member/MyPage.js
+++ thefesta/src/member/MyPage.js
@@ -11,9 +11,11 @@ function MyPage() {
 
   const [imageUrl, setImageUrl] = useState("");
   const fileInputRef = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const id = Cookies.get('loginInfo').trim();
   const parsedId = id ? JSON.parse(id) : '';
   const navigate = useNavigate();
+  const [nickname, setNickname] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,9 +23,9 @@ function MyPage() {
         const response = await axios.post('http://localhost:9090/member/selMember', {
           id: parsedId
         });
-        
-        const memData = response.data;
 
+        const memData = response.data;
+        setNickname(memData.nickname);
         if (memData.profileImg) {
           setImageUrl(memData.profileImg);
         }
@@ -40,9 +42,9 @@ function MyPage() {
 
   const handleFileChange = async (event) => {
     const selectedFile = event.target.files[0];
-    
+
     const formData = new FormData();
-    formData.append('id', parsedId); 
+    formData.append('id', parsedId);
     formData.append('file', selectedFile);
 
     try {
@@ -57,7 +59,7 @@ function MyPage() {
           id: parsedId,
           randomParam: Math.random(),
         });
-        
+
         const memData = memberResponse.data;
         console.log("memData : " + memData.profileImg);
 
@@ -72,48 +74,50 @@ function MyPage() {
     }
   };
 
-  const handleLogout = async () => {
-        try {
-            const response = await axios.get('http://localhost:9090/member/logout', {
-                params: { id }
-            });
-    
-            if (response.data === 'success') {
-              navigate('/')
-              removeCookie('loginInfo');
-            } else {
-                console.error('로그아웃 실패:', response.data);
-            }
-        } catch (error) {
-            // 오류 처리
-            console.error('로그아웃 중 오류 발생:', error);
-        }
-    };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <div>
-      <img
-        src={`${imageUrl}`}
-        alt="Profile Preview"
-        onClick={handleImageClick}
-        className='mypageimage'
-      />
+    <div className='MyPage-myPage'>
+      <h1 className='MyPage-title'>마이페이지</h1>
+      <div className='MyPage-container'>
+        <div className='MyPage-menuBtn'>
+          <img className='MyPage-kebabBtn' src='./images/kebab_btn.png' onClick={toggleMenu}></img>
+        </div>
 
-      <input
-        type="file"
-        ref={fileInputRef}
-        onChange={handleFileChange}
-        style={{ display: 'none' }}
-      /><br/>
+        {isMenuOpen && (
+          <div className='MyPage-selectMenu'>
+            <div className='MyPage-box'>
+              <img src='./images/memInfoReset.png' />
+              <Link to='/MemInfoReset' className='MyPage-memInfoReset'>회원 정보 수정</Link>
+            </div>
+            <div className='MyPage-box'>
+              <img src='./images/user_basic.png' />
+              <Link to='/withdrawal' className='MyPage-withdrawal'>회원 탈퇴</Link>
+            </div>
+          </div>
+        )}
 
-      <>
-        <Link to='/MemInfoReset' className='MemInfoReset'>회원정보수정</Link><br/>
-        <Link to='/withdrawal' className='withdrawal'>회원탈퇴</Link>
-      </>
+        <div className='MyPage-image'>
+          <img className='MyPage-profileImg' src={imageUrl} alt="Profile Preview" onClick={handleImageClick} />
+          <input className='MyPage-uploadImg' type="file" ref={fileInputRef} onChange={handleFileChange} style={{ display: 'none' }} />
+        </div>
+        <div className='MyPage-nickname'>
+          <p className='MyPage-name'>{nickname} 님</p>
+        </div>
 
-      <button type="button" onClick={handleLogout} className='PwReset-button'>
-          로그아웃
-        </button>
+        <div className='MyPage-activity'>
+          <div className='MyPage-festival'>
+            <Link to={`/member/likeList/${parsedId}`}><img className='MyPage-likeFesta' src='./images/star.png' alt='좋아요' /></Link>
+            <Link to={`/member/likeList/${parsedId}`}><p>좋아요</p></Link>
+          </div>
+          <div className='MyPage-board'>
+            <Link to='/member/talktalk'><img className='MyPage-talktalk' src='./images/ttalk.png' alt='톡톡' /></Link>
+            <Link to='/member/talktalk'><p>톡톡</p></Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
