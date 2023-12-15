f1501c53f88e49c309eb44c2a6d33e0037360df0
irawa9 <irawa@naver.com>
2023-12-15 14:31:55 +0900

    23.12.15 RSI board update

---
+++ thefesta/src/board/BoardPage.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import BoardList from './components/BoardList';
+
+const BoardPage = () => {
+
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <>
+      <BoardList></BoardList>
+    </>
+  );
+};
+
+export default BoardPage;
