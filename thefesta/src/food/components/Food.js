e5c1934ca372f254b88c057b1e644cef0313e95e
PMR <12121db@gmail.com>
2023-12-24 23:11:25 +0900

    2023.12.24 PMR css modified, fooddetail page open in new window function add

--- thefesta/src/food/components/Food.js
+++ thefesta/src/food/components/Food.js
@@ -16,18 +16,19 @@ function Food({ contentid, title, addr1, firstimage2 }) {
     // title 글자 수에 따라 클래스네임 설정
     const titleClassName = title.length > 10 ? 'Food-title-long' : 'Food-title';
 
-    return (
-        <div className="Food">
-            <Link to={`/food/detail/${contentid}`}>
-                <img src={imageSource} title={title} alt={title} />
-            </Link>
-            <div className="Food-data">
-                <h3 className={titleClassName}>
-                    <Link to={`/food/detail/${contentid}`}>{title}</Link>
-                </h3>
-                <h5 className="Food-addr"><Link to={`/food/detail/${contentid}`}>{addr1}</Link></h5>
-            </div>
+    // 상세 페이지 새 창에서 열기
+    const handleFoodClick = () => {
+        const detailedPageURL = `/food/detail/${contentid}`;
+        window.open(detailedPageURL, '_blank');
+    };
 
+    return (
+        <div className="Food" onClick={handleFoodClick}>
+            <img src={imageSource} title={title} alt={title} />
+            <div className="Food-data">
+                <h3 className={titleClassName}>{title}</h3>
+                <h5 className="Food-addr">{addr1}</h5>
+            </div>
         </div>
     )
 }
