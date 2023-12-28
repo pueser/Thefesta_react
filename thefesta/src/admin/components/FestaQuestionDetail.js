8555fea228938aab8b1bc406aab3c27264b347fd
pueser <hwanghyejin932@gmail.com>
2023-12-28 09:41:51 +0900

    admin push

--- thefesta/src/admin/components/FestaQuestionDetail.js
+++ thefesta/src/admin/components/FestaQuestionDetail.js
@@ -18,7 +18,7 @@ function FestaQuestionDetail (){
         }).then((response)=>{
             console.log(response);
             alert(`${response.data}번 건의글이 확인되었습니다`)
-            navigate(`/Question/${contentid}`)
+            return
 
         }).catch((error)=>{
             console.log(error)
@@ -34,7 +34,7 @@ function FestaQuestionDetail (){
                 <div className="adminDetailReportContent">건의내용</div>
                 <div className="adminDetailOut"><Link to={{pathname:`/admin/festaQuestion/${contentid}`}} className="adminLinkBtn">X</Link></div>
             </div>
-            <div className="adminReportContent">{questioncontent}</div>
+            <div className="adminReportContent" style={{ overflowY: 'auto', maxHeight: '380px' }}>{questioncontent}</div>
             <div className="adminDetailBtn">
                 <Link to={`/admin/festaQuestion/${contentid}`}><button onClick={()=>deleteClick(questionid)} className="adminApprove-button">확인완료</button></Link>
                 <Link to={`/admin/festaQuestion/${contentid}`}><button  className="adminApprove-button">확인취소</button></Link>
